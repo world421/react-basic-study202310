23a6e7a60bda7411dd77d86f3938410c6678922d
KangNuri <nuris0421@gmail.com>
2023-10-31 12:49:19 +0900

    2. 동적 데이터 렌더링 및 props 개념 확인

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import './App.css';
 import Expenseltem from './components/Expenseltem';
-import NoName from './noname';
 
-function App() {
-  const $h2 = <h2> 반가워용 ! </h2>;
+const App = () => {
+  //지출 항목 객체 배열
 
+  const expense = [
+    {
+      title: '바나나',
+      price: 2000,
+      date: new Date(2023, 3, 23),
+    },
+    {
+      title: 'BBQ 치킨',
+      price: 20000,
+      date: new Date(2023, 5, 21),
+    },
+    {
+      title: '도미노피자',
+      price: 35000,
+      date: new Date(2023, 7, 4),
+    },
+  ];
   return (
     <>
-      <NoName />
-      <NoName />
-      <NoName />
-      <NoName />
-      <div className='App'>
-        <h1> 메롱메롱 </h1>
-        {$h2}
-      </div>
-      <div className='noname'>
-        <input type='text' />
-        <p>
-          오늘은 월요일 입니다. <br />
-          그래서 공부하기 정말정말정말 시러용
-        </p>
-      </div>
+      <Expenseltem
+        title={expenses[0].title}
+        price={expense[0].price}
+        date={expense[0].date}
+      />
+      <Expenseltem
+        title={expenses[1].title}
+        price={expense[1].price}
+        date={expense[1].date}
+      />
+      <Expenseltem
+        title={expenses[2].title}
+        price={expense[2].price}
+        date={expense[2].date}
+      />
     </>
   );
-}
+};
 
 // div 태그 2개 한번에 리턴 안됨 !
 export default App;
+// 속성의 이름은 정해진게 없다
 // APP EXPORT(내보내겠다 import 하는 곳으로 )
--- src/components/Expenseltem.js
+++ src/components/Expenseltem.js
@@ -1,14 +1,41 @@
 import React from 'react';
+// css 로딩
+import './ExpenseItem.css';
 
-const Expenseltem = () => {
+const Expenseltem = ({ title, price: propsPrice, date }) => {
+  //console.log(props);
+  //  부모가 준 값으로 ! props!
+  // const price = 99999;
+  // const expenseDate = date;
+  // const expenseTitle = title;
+  // const expensePrice = propsPrice;
+
+  // 1자리 숫자를 2자리수로 변환하는 함수
+  const make2digit = (text) => {
+    return text.toString().padStart(2, '0');
+  };
+
+  // 날짜 포맷팅 변환 함수 정의
+  const makeFormattedDate = () => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
+  };
+
+  // 숫자를 원화 표기법으로 바꾸기
+  const formattedPrice = new Intl.NumberFormat('ko-KR').format(expensePrice);
+
   return (
     <div className='expense-item'>
-      <div>2023년 10월 30일</div>
+      <div>{makeFormattedDate()}</div>
       <div className='expense-item__description'>
-        <h2>냠냠치킨</h2>
-        <div className='expense-item__price'>19000원</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>{formattedPrice}원</div>
       </div>
     </div>
   );
 };
 export default Expenseltem;
+
+// 자바스크립트의 변수명이라는 걸 알려주기 위해서는  {} 사용함!
