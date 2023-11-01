a0dbbd26f4bab353e2bbc72dc1430e903087691a
KangNuri <nuris0421@gmail.com>
2023-11-01 14:56:24 +0900

    8. 상태 끌어올리기

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ onSaveExpense }) => {
+  const [userInput, setUserInput] = useState({
+    title: '',
+    price: '',
+    date: '',
+  });
+
+  const titleChangeHandler = (e) => {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        title: e.target.value,
+      };
+    });
+
+    // ( ) 주겠다.
+  };
+  const priceChangeHandler = (e) => {
+    setUserInput({
+      // userInput에 있는 객체 프로퍼티 그대로 복사해서 붙여넣겠다는거
+      ...userInput,
+      price: e.target.value,
+    });
+  };
+
+  const dateChangeHandler = (e) => {
+    setUserInput({
+      ...userInput,
+      date: e.target.value,
+    });
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault(); // submit차단!
+
+    onSaveExpense(userInput);
+
+    // 입력창 리셋
+    setUserInput({
+      title: '',
+      price: '',
+      date: '',
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input
+            type='text'
+            onChange={titleChangeHandler}
+            value={userInput.title}
+            //상태변수 변화 감지 하면 리렌더링해쥼
+          />
         </div>
         <div className='new-expense__control'>
           <label>Price</label>
@@ -15,6 +64,8 @@ const ExpenseForm = () => {
             type='number'
             min='100'
             step='100'
+            onChange={priceChangeHandler}
+            value={userInput.price}
           />
         </div>
         <div className='new-expense__control'>
@@ -23,6 +74,8 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max='2025-12-31'
+            onChange={dateChangeHandler}
+            value={userInput.date}
           />
         </div>
       </div>
