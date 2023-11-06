82d4679e428785868abb67adf86d249462ed9a24
KangNuri <nuris0421@gmail.com>
2023-11-06 10:59:37 +0900

    에러상태핸들링

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUsers from './components/Users/AddUsers';
 import UserList from './components/Users/UserList';
 
 const App = () => {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (user) => {
+    //prev 는 가장 최신의 값을 줌
+    setUserList((prev) => [...prev, { ...user, id: Math.random().toString() }]);
+  };
   return (
     <div>
-      <AddUsers />
-      <UserList />
+      <AddUsers onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 };
