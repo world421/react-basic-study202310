91cbbf1d91d595143d385ee7b0b098883da301e1
KangNuri <nuris0421@gmail.com>
2023-11-07 13:20:12 +0900

    useEffect cleanup함수

--- src/components/SideEffect/Login/Login.js
+++ src/components/SideEffect/Login/Login.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../../UI/Card';
 import styles from './Login.module.css';
 import Button from '../../UI/Button/Button';
 
 const Login = ({ onLogin }) => {
-    //이메일 입력값을 저장
+  //이메일 입력값을 저장
   const [enteredEmail, setEnteredEmail] = useState('');
   // 이메일 입력이 정상적인지 확인
   const [emailIsValid, setEmailIsValid] = useState();
@@ -16,20 +16,31 @@ const Login = ({ onLogin }) => {
   //이메일, 패스워드가 둘 다 동시에 정상적인 상태인지 확인
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // 입력란을 모두 체크하여 form의 버튼 disabled를 해제하는 상태 변수
+  // formIsValid의 사이드 이펙트를 처리하는 영역
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('useEffect called in Login.js');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 1000);
+
+    //cleanup 함수 -컨포넌트가 업데이트 되거나 없어지긴 전에 실행
+    // 리렌더링되기 전에마지막으로 호출
+    return () => {
+      console.log('clean up !');
+      clearTimeout(timer);
+    };
+    // 이 배열에 상태변수를 넣어주면 그 상태변수가 바뀔 때마다 useEffect를 재실행함 !.
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (e) => {
     setEnteredEmail(e.target.value);
-
-    setFormIsValid(
-      e.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (e) => {
     setEnteredPassword(e.target.value);
-
-    setFormIsValid(
-      e.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
